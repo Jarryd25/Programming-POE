68cb941b91725011f3a497440bc90883a5948010
Jarryd O'Neill <st10018137@imconnect.edu.za>
2023-04-22 12:20:49 +0200

    I began by creating the namespace, and two classes. The Ingredients class will handle the ingredients and the Recipe class will handle user input.

--- Part 1/Program.cs
+++ Part 1/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+
+namespace RecipeAndIngredients
+{
+    // Define a class for an ingredient
+    class Ingredient
+    {
+
+    }
+
+    // Define a class for a recipe
+    class Recipe
+    {
+        
+    }
+}
